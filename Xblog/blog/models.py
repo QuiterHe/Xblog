1c09703353abb6e5c2cc473860d05e63ed41c495
zhang.he <zhang.he@wenba100.com>
2017-09-26 10:20:49 +0800

    动态文章列表首页

--- Xblog/blog/models.py
+++ Xblog/blog/models.py
@@ -1,5 +1,21 @@
-from __future__ import unicode_literals
-
+# from __future__ import unicode_literals
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+	author = models.IntegerField()                                 
+	title = models.CharField(max_length=200)                       
+	text = models.TextField()                                      
+	created_data = models.DateTimeField(default=timezone.now)      
+	published_data = models.DateTimeField(blank=True, null=True)   
+
+	"""docstring for Post"""
+	def publish(self):
+		# self.published_data = timezone.now()
+		self.published_data = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
+
--- Xblog/blog/views.py
+++ Xblog/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+	posts = Post.objects.filter(published_data__isnull=False).order_by('-published_data')
+	return render(request, 'blog/post_list.html', {'posts':posts})
